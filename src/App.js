6557f9de62ca7145cca6ec3dd218158ac413a944
Mohit Jamle <95996704+MohitJamle@users.noreply.github.com>
2022-03-13 16:28:37 +0530

    this is final commit

--- src/App.js
+++ src/App.js
@@ -1,36 +1,14 @@
-import axios from "axios";
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import "./App.css";
 import SearchBar from "./components/SearchBar";
+import { featchData } from "./store/index";
 
 function App() {
   const dispatch = useDispatch();
 
   useEffect(() => {
-    const fetchData = async () => {
-      const sendRequest = async () => {
-        const albumData = await axios
-          .get(`https://jsonplaceholder.typicode.com/albums`)
-          .catch((error) => {
-            console.log(error);
-          });
-        const photosData = await axios
-          .get(`https://jsonplaceholder.typicode.com/photos`)
-          .catch((error) => {
-            console.log(error);
-          });
-
-        return {
-          type: "Add",
-          albums: albumData.data,
-          photos: photosData.data,
-        };
-      };
-      const fullData = await sendRequest();
-      dispatch(fullData);
-    };
-    fetchData();
+    dispatch(featchData());
   }, [dispatch]);
   return (
     <div>
