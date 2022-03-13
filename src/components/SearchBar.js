fc431a7b50b548e853e55f01646b388e667e1fc5
Mohit Jamle <95996704+MohitJamle@users.noreply.github.com>
2022-03-13 16:25:45 +0530

    this is search commit

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -9,7 +9,7 @@ import { makeStyles } from "@mui/styles";
 const useStyles = makeStyles({
   input: {
     backgroundColor: "#eee5ff",
-    width: "80%",
+    width: "80vw",
     padding: "5px 20px",
     marginTop: "15px",
     marginLeft: "9vw",
@@ -31,24 +31,19 @@ const useStyles = makeStyles({
   h2: {
     fontSize: "20px",
     fontWeight: "600",
-    marginLeft: "135px",
+    marginLeft: "136px",
     marginBottom: "0px",
   },
   time: {
     color: "grey",
-    position: "relative",
+    position: "absolute",
     top: "20px",
-    left: "43px",
-    fontSize: "10px",
+    right: "285px",
+    fontSize: "13.5px",
   },
   val: {
-    position: "relative",
-    left: "370px",
-    color: "green",
-  },
-  va: {
-    position: "relative",
-    left: "370px",
+    position: "absolute",
+    right: "300px",
     color: "green",
   },
 });
@@ -57,26 +52,26 @@ const SearchBar = (props) => {
   const albums = useSelector((state) => state.albumData);
   const photos = useSelector((state) => state.photoData);
   const [searchItem, setSearchItem] = useState("");
+  const [searchItems, setSearchItems] = useState("");
   const classes = useStyles();
 
   const inputChangeHandler = (event) => {
     setSearchItem(event.target.value);
+    setSearchItems(searchItem.toLocaleLowerCase());
   };
 
   const items = albums.map((item) => {
     if (item.id < 6) {
       return (
-        <div>
+        <div key={item.id}>
           <h3 className={classes.h2} key={item.id}>
             {item.title}
           </h3>
-
           {photos.map((photo) => {
             // if (photo.albumId === item.id) {
-
             if (photo.id < 11) {
               const randomNumber = Math.floor(Math.random() * 200) + 50;
-              if (photo.title.includes(searchItem)) {
+              if (photo.title.includes(searchItems)) {
                 return (
                   <ListItem className={classes.list} key={photo.id}>
                     <img className={classes.img} src={photo.url} alt="" />
@@ -94,17 +89,14 @@ const SearchBar = (props) => {
                         }}
                         primary={"$ " + randomNumber}
                       />
-                      <ListItemText
-                        className={classes.time}
-                        primary={"10.00 AM"}
-                      />
+                      <span className={classes.time}>10.00 AM</span>
                     </ListItem>
                   </ListItem>
                 );
               }
             }
             // }
-            // return null;
+            return console.log();
           })}
         </div>
       );
